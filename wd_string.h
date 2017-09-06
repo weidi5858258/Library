b2390a729ef2bc1179e838beca6607883c7f6c90
SGA-Liwei-Wang <xLiwei.Wang@sony.com>
2017-09-06 23:12:16 +0800

    201709062312

--- wd_string.h
+++ wd_string.h
@@ -5,5 +5,12 @@ void wd_memcpy(void);
 void wd_memmove(void);
 void wd_memchr(void);
 void wd_memcmp(void);
+void wd_memset(void);
+void wd_strcat(void);
+void wd_strchr(void);
+void wd_strcmp(void);
+void wd_strcoll(void);
+void wd_strcpy(void);
+void wd_strcspn(void);
 
 #endif // TIME_H
